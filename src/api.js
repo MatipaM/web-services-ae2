3cb0edcf66111b7dbfb00afdf36b12cbf3597eb0
Matipa Marufu <2344503@students.wits.ac.za>
2024-08-03 08:22:47 +0100

    subscribed feeds finished
    needs login to be tested

--- src/api.js
+++ src/api.js
@@ -50,13 +50,13 @@ export async function saveArticle(email, articleName, url) {
   return response.json();
 }
 
-export async function saveFeed(feed_name, url) {
-  const response = await fetch(`${API_URL}/article`, {
+export async function saveFeed(email, feed_name, url) {
+  const response = await fetch(`${API_URL}/feed`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ feed_name: feed_name, url }),
+    body: JSON.stringify({ email, feed_name: feed_name, url }),
   });
   if (!response.ok) {
     throw new Error('Failed to save article');
